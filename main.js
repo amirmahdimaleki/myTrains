c2319fd94ca88a2244fdd261abc81d94e320ea5b
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-01-16 21:58:29 +0330

    c9

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 document.querySelectorAll(".mainH1").textContent = "Together we grow!";
+const phoneModel = "xiaomi";
 const phone = {
+  phoneModel: "mi note 10 lite",
   socialNetwork: 4,
   education: 3,
   games: [pubg, newState, race],
-  os,
+  os: "android",
+  features: {
+    cpu: "sd730g",
+    gpu: "aderno 618",
+  },
 };
